f0cf727b7ba3b034641c25a642bfddd47709b6d1
adomakor412 <ronaldadomako@icloud.com>
2020-10-27 00:44:42 -0400

    Machine Learning WIP, for loop ofnpy files for rows in train-test sets

---
+++ MachineLearning.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.linear_model import SGDClassifier, Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import LogisticRegression
