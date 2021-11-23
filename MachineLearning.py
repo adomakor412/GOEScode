67964e5146fa0261859f81454fc0b84bfea98933
Ronald Adomako <ronaldadomako@icloud.com>
2021-11-23 23:33:42 +0000

    Reset of git files

--- MachineLearning.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.filterwarnings('ignore')
-from sklearn.linear_model import SGDClassifier, Perceptron
-from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.linear_model import LogisticRegression
